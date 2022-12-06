a8353c63e6e206bded8451ba4a0bf1a9933c2904
clbokea <clbo@kea.dk>
2022-12-06 21:36:30 +0100

    home icon changed to 16 from 24

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $().ready(function() {
 	$(".page-header").attr('style', 'display:none')
 	$(".page-header a:first").hide()
-	$(".main-content").prepend("Teknologi 1 - KEA forår 2023 - Kompendie<div style='text-align: right'><a href='/f23/'><img src='https://techkea.github.io/f23/img/home-5-24.png'></a>");
+	$(".main-content").prepend("Teknologi 1 - KEA forår 2023 - Kompendie<div style='text-align: right'><a href='/f23/'><img src='https://techkea.github.io/f23/img/home-5-16.png'></a>");
 	
 
 	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br> Claus Bove (clbo@kea.dk) forår 2023 <br> <div>")
