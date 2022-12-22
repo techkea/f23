bb85c5d1992432f0625595da19748875989e186d
clbokea <clbo@kea.dk>
2022-12-22 10:52:56 +0100

    script.js adjusted to no template style

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 $().ready(function() {
-	$(".page-header").attr('style', 'display:none')
-	$(".page-header a:first").hide()
-	$(".main-content").prepend("Teknologi 1 - KEA forår 2023 - Kompendie <!--<div style='text-align: right'><a href='/f23/'><img src='https://techkea.github.io/f23/img/home-5-16.png'></a>-->");
-	
-
-	$(".site-footer").append("<div style='text-align:center'><small>Licensed under <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>CC BY-NC-SA.</a> <br> Claus Bove (clbo@kea.dk) forår 2023 <br> <div>")
-	$(".site-footer-owner").hide()
-        $(".site-footer-credits").hide()	
-	$(".main-content").attr('style', 'max-width:60rem; font-size:1.0rem');
-
+	$("h1:first").hide()
+	$(".markdown-body").append("<div style='text-align:center; margin-top:40px'><small>Teknologi 1 - KEA forår 2023 - Kompendie - clbo@kea.dk</small></div>");
 });
 
 
