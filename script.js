0e3df02b7a84368b48cb84b86febec0e68d53f97
clbokea <clbo@kea.dk>
2022-12-30 17:06:44 +0100

    text color to #666666

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $().ready(function() {
 	$("h1:first").hide()
 
-	$(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0'><small>Teknologi 1 - KEA forår 2023 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#666666'><small>Teknologi 1 - KEA forår 2023 - Kompendie - clbo@kea.dk</small></div>");
 
-	$(".markdown-body").append("<div style='text-align:center; margin-top:60px'><small>Teknologi 1 - KEA forår 2023 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").append("<div style='text-align:center; margin-top:60px; color:#666666'><small>Teknologi 1 - KEA forår 2023 - Kompendie - clbo@kea.dk</small></div>");
 });
 
 
